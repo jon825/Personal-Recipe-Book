7c7cfbe322a351d6a9251af5b70b9ca622fbdf80
Jonathan Kok <jon.825@Jonathans-MacBook-Pro.local>
2017-12-13 03:09:07 -0800

    test1

--- back_end/server.js
+++ back_end/server.js
@@ -10,7 +10,11 @@ app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x
 const recipes_routes = require('./routes/recipes');
 
 
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.use('/api/recipes', recipes_routes);
 
